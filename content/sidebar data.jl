630c4c3e7169b77deae7bbabc90055167ced6650
HaneWall <wallneremail@gmail.com>
2023-10-04 10:22:42 +0200

    Update term
    test

--- content/sidebar data.jl
+++ content/sidebar data.jl
@@ -13,7 +13,7 @@ Dict(
         ],
         :title => "A Great Course",
         :subtitle => "Even Greater Than You Thought",
-        :term => "Spring 2023",
+        :term => "Winter 2023",
         :institution => "University of Greatness",
         :institution_url => "http://www.uva.nl",
         :institution_logo => "julia-logo-color.svg",
