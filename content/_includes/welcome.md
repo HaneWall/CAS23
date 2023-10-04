062bfaf9a39d18639716e81dabf82b878fc975a3
HaneWall <wallneremail@gmail.com>
2023-10-04 11:22:12 +0200

    new sidebar, welcome, basic_syntax

--- content/_includes/welcome.md
+++ content/_includes/welcome.md
@@ -2,12 +2,13 @@
 layout: "md.jlmd"
 ---
 
-# Julia-Praktika: Computerorientierte Mathematik, Algorithmen und Strukturen
+# Julia-Praktika: CAS
 
 Willkommen in der Julia-Übungsgruppe der CAS-Vorlesung im Wintersemester 23/24. Auf dieser Homepage werdet ihr eine Übersicht
 zur Julia-Syntax erhalten. Zudem werden hier eure Pluto-Notebooks zur Bearbeitung der Aufgaben bereitgestellt.
 
-HIER eventuell eine kleine geschihte zu Julia.
+Ich wünsche euch viel Erfolg,
+cheers Hannes
 
 > **Acknowledgement** \\
 > The design of this website is based on _**Computational Thinking**, a live online Julia/Pluto textbook._ [(computationalthinking.mit.edu)](https://computationalthinking.mit.edu)
--- content/sidebar data.jl
+++ content/sidebar data.jl
@@ -8,14 +8,14 @@ Dict(
     ],
     :about => Dict(
         :authors => [
-            (name = "Instructor One", url = "https://www.the-one.eu"),
-            (name = "Second Instructor", url = "https://www.the-second.com")
+            (name = "Prof. Jens Starke", url = "http://www.jstarke.eu"),
+            (name = "Hannes Wallner", url = "https://www.the-second.com")
         ],
-        :title => "A Great Course",
-        :subtitle => "Even Greater Than You Thought",
-        :term => "Winter 2023",
-        :institution => "University of Greatness",
-        :institution_url => "http://www.uva.nl",
+        :title => "CAS Julia",
+        :subtitle => "Eine interaktive Einführung",
+        :term => "Winter 2023/2024",
+        :institution => "Universität Rostock",
+        :institution_url => "http://www.uni-rostock.de",
         :institution_logo => "julia-logo-color.svg",
         :institution_logo_darkmode => "julia-logo-dark.svg"
     )
