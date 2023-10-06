9a9fa162797330f874b212a0bcaff225953a554e
HaneWall <48854946+HaneWall@users.noreply.github.com>
2023-10-06 15:38:32 +0000

    Deploying to output from  @ d47ce039bb3352d9383597f997b07fc74e4f5866 🚀

--- Fall23/generated_assets/basic_syntax_67ccad0a.jl
+++ Fall23/generated_assets/basic_syntax_67ccad0a.jl
@@ -15,10 +15,9 @@ using InteractiveUtils
 
 # ╔═╡ 0d3aec92-edeb-11ea-3adb-cd0dc17cbdab
 md"# Erste Schritte in Julia
+✋ Bevor Ihr hier anfangt, solltet Ihr vermutlich zunächst Julia installieren und das Package Pluto herunterladen. Eine genaue Anleitung findet Ihr weiter oben unter dem Reiter Software Installation :).
 
-Bevor Ihr 
-
-Before being able to run this notebook successfully locally, you will need to [set up Julia and Pluto.](/Spring21/installation/)
+Ready? 🎬 Dann hier eine kleine Einführung: 
 "
 
 
@@ -26,7 +25,7 @@ Before being able to run this notebook successfully locally, you will need to [s
 md"""
 ## Variablen
 
-Wir können mithilfe des `=` Zuweisungsoperators Variablen definieren. Einmal festgelegt können wir dann die Größe auch in anderen Zellen nutzen.
+Wir können mithilfe des `=` Zuweisungsoperators Variablen definieren. Dabei wird immer der linken Variablen der rechte Wert zugewiesen. Einmal festgelegt können wir dann die Größe/Variable auch in anderen Zellen nutzen.
  """
 
 # ╔═╡ 3e8e0ea0-edeb-11ea-22e0-c58f7c2168ce
@@ -128,7 +127,7 @@ Nutze `for` zum iterieren über ein `Iterator` / `Array` / `Range` und dessen Va
 
 # ╔═╡ fe3fa290-edeb-11ea-121e-7114e5c573c1
 let 
-# Dies ist ein Kommentar! :)
+# Dies ist ein Kommentar! :) (Kennzeichunng mittels #)
 s = 0
 # Summation aller Zahlen von 1 bis 10:
 for i in 1:10
@@ -192,86 +191,266 @@ Wir sehen, dass Bedingungen einen `Bool` Ausgabetypen (`true` oder `false`) besi
 # ╔═╡ bc6b124e-eded-11ea-0290-b3760cb81024
 if a < 5
 	"small"
-	
 else
 	"big"
-	
 end
 
 # ╔═╡ cfb21014-eded-11ea-1261-3bc30952a88e
-md"""Note that the `if` also returns the last value that was evaluated, in this case the string `"small"` or `"big"`, Since Pluto is reactive, changing the definition of `a` above will automatically cause this to be reevaluated!"""
+md"""
+Falls diese Verzweigung auch noch nicht genügt, kann man eine `if - elseif - else - end` Umgebung basteln.
+"""
+
+# ╔═╡ 0ba17e45-bdca-40de-af10-e4167251a5f7
+if a < 2
+	"small"
+elseif a >= 2 && a <= 4 
+    "intermediate"
+else a>4
+	"big"
+end
+
+# ╔═╡ 22d6c86e-fbf2-4246-aee1-afe700b49355
+md"""
+Hierbei haben wir Gebrauch von den `&&` Vergleichsoperator gemacht. Dieser vergleicht zwei boolsche Bedingungen (links und rechts vom Operator). Sind beide wahr (`true`), so ist die Ausgabe abermals `true`. 
+
+Generell kann man sogar mehrere `elseif` Abfragen in einen solcher Umgebung unterbringen.
+"""
 
 # ╔═╡ ffee7d80-eded-11ea-26b1-1331df204c67
-md"## Arrays"
+md"## `Array`s"
+
+# ╔═╡ daeca7d5-0100-41ea-a106-6b1a7ecdcf7a
+md"""
+`Array`s sind gewissermaßen das Fundament der computerorientierten Mathematik. Sie erlauben es uns mehrere Werte zu speichern und auszulesen.
+"""
 
 # ╔═╡ cae4137e-edee-11ea-14af-59a32227de1b
-md"### 1D arrays (`Vector`s)"
+md"### 1D `Arrays`"
 
 # ╔═╡ 714f4fca-edee-11ea-3410-c9ab8825d836
-md"We can make a `Vector` (1-dimensional, or 1D array) using square brackets:"
+md"""
+Wir können ein eindimensionales `Array` (1-dimensional, oder 1D `Array`) durch eckige Klammern `[ ]` erzeugen.
+"""
 
 # ╔═╡ 82cc2a0e-edee-11ea-11b7-fbaa5ad7b556
-v = [1, 2, 3]
+v = [1, 3, 5]
 
 # ╔═╡ 85916c18-edee-11ea-0738-5f5d78875b86
 typeof(v)
 
 # ╔═╡ 881b7d0c-edee-11ea-0b4a-4bd7d5be2c77
-md"The `1` in the type shows that this is a 1D array.
+md"""
+Die `1` im Typen zeigt die Anzahl der Dimensionen. Ausgesprochen: `v` ist ein 1D array, welches Elemente vom Typen `Int64` beherbergt.
 
-We access elements also using square brackets:"
+Fundamental bei Arrays: Wir können deren Elemente einsehen und ändern. Zum Beispiel können wir durch `array[index]` uns das Element an der Position `index` des Arrays ausgeben lassen.
+"""
 
 # ╔═╡ a298e8ae-edee-11ea-3613-0dd4bae70c26
 v[2]
 
+# ╔═╡ 98664fb6-ebaf-449a-9f20-aaa3f955f44e
+md"""
+Natürlich können wir auch das Element an zweiter Stelle ändern: 
+"""
+
 # ╔═╡ a5ebddd6-edee-11ea-2234-55453ea59c5a
 v[2] = 10
 
-# ╔═╡ a9b48e54-edee-11ea-1333-a96181de0185
-md"Note that Pluto does not automatically update cells when you modify elements of an array, but the value does change."
+# ╔═╡ b6a3fe2e-7504-4d94-9672-ef564308d387
+v
+
+# ╔═╡ 3c7424db-51c9-41a2-9630-2c188a80d5d5
+md"""
+### Nett zu wissen und noch häufiger zu gebrauchen:
+"""
 
 # ╔═╡ 68c4ead2-edef-11ea-124a-03c2d7dd6a1b
-md"A nice way to create `Vector`s following a certain pattern is to use an **array comprehension**:"
+md"""
+Eine äußerst elegante Art und Weise `Array`s zu erzeugen ist das sogenannte **array comprehension** Muster. Dabei verbinden wir eine For-Schleife mit der Konstruktion eines `Array`s. 
+
+Z.B. alle Quadratzahlen von 1 bis 10 als 1D `Array`: 
+"""
 
 # ╔═╡ 84129294-edef-11ea-0c77-ffa2b9592a26
 v2 = [i^2 for i in 1:10]
 
+# ╔═╡ 0025dea4-99cc-4cb4-9c92-f14d04af56eb
+md"""
+Die Alternative: Zunächst erstellen wir ein 10-Elemente langes Array aus Nullen vom Typ `Int64`.
+"""
+
+# ╔═╡ de160dfe-53ef-4788-86df-157b44e5e1da
+v3 = zeros(Int64, 10)
+
+# ╔═╡ 4341aee5-0631-4171-953c-2ba07e0ee135
+md"""
+Anschließend iterieren wir mittels `for` Schleife über die `Indizes` des `Array`s und verändern die Elemente zu den gewünschten Quadratzahlen.
+"""
+
+# ╔═╡ 3e362465-9e27-45b2-a062-757cbde31eca
+begin 
+for idx in 1:10 
+	v3[idx] = idx^2
+end
+v3
+end
+
+# ╔═╡ cbd56fc2-1c96-45fb-99a9-2deb68a0e2b9
+md"""
+### `Array`-Slicing:
+"""
+
+# ╔═╡ 2a23488b-49bf-4743-9501-21030acc2139
+md"""
+Sagen wir, wir möchten ein Stück aus dem ehemaligen `Array` extrahieren. Spezifischer wir möchten ein neues Array `v4` erzeugen welches dem ursprünglichen `v3` vom `Index` 3 bis 7 entspricht (Grenzen inkludiert). Dies können wir durch den `:` Operator innerhalb der eckigen Klammer gewährleisten. 
+
+"""
+
+# ╔═╡ c386d317-3fdd-4088-b5e7-cc7dadd0b518
+v4 = v3[3:7]
+
+# ╔═╡ b55c6ef3-c81d-4f6a-8f0f-05044d751a9d
+md"""
+Ab und an kommt es vor, dass man eventuell nur jedes `n`-te Element des ursprünglichen `Array`s benötigt. Dafür nutzen wir dann die **slicing**-Syntax, bei der wir zwischen den `Start` und `Stop` `Index` zusätzlich eine Schrittweite `Step` hinzufügen. 
+
+`array[Start:Step:Stop]` . Z.B. nur jedes `2`-te Element des Quadratzahlenarrays:
+"""
+
+# ╔═╡ b26ff5b6-6e68-40e6-9ef1-afa2105f268b
+v5 = v3[1:2:10]
+
+# ╔═╡ 45bba432-2b07-4a72-87e4-688bb2ca40a9
+md"""
+### Konstruktion von `Arrays` mit festgelegter Schrittweite
+"""
+
+# ╔═╡ d5406f4c-023b-4e96-9707-10436b96df56
+md"""
+Mithilfe der **Slicing**-Syntax können wir in Julia auch `Array`s konstruieren. Zunächst bastelt man sich eine **range** mittels `r = Start:Step:Stop`, diese kann dann anschließend mittels `collect(r)` zu einen `Array` umgeformt werden. 
+
+Zum Beispiel hier die Erzeugung eines 1D `Array`s, dessen Elemente von 1 bis 19 mit einer Schrittweite von 3 reichen. 
+"""
+
+# ╔═╡ d13638ef-1837-44a0-afb9-7bedcb7187b9
+arr = collect(1:3:19)
+
 # ╔═╡ d364fa16-edee-11ea-2050-0f6cb70e1bcf
-md"## 2D arrays (matrices)"
+md"## 2D `Arrays`"
 
 # ╔═╡ db99ae9a-edee-11ea-393e-9de420a545a1
-md"We can make small matrices (2D arrays) with square brackets too:"
+md"""
+1D `Array`s werden schnell langweilig. Lasst uns nun 2D `Array`s anschauen, die sehr häufig benötigt werden. Ihr werdet feststellen das mit dem Vorwissen von 1D `Array`s der Sprung zu ND `Array`s nur minimal ist. Man kann auf ganz verschiedene Art und Weisen ein 2D `Array` bzw. eine Matrix erzeugen. Alle sind äquivalent:
+"""
 
 # ╔═╡ 04f175f2-edef-11ea-0882-712548ebb7a3
 M = [1 2
 	 3 4]
 
+# ╔═╡ bc19f4aa-404f-48df-ad5f-bfceed049286
+M₂ = [1 2; 3 4]
+
+# ╔═╡ a3881a8c-1bbd-4e42-96af-f8b5786c9cac
+md"""
+Mittels Spaltenvektoren:
+"""
+
+# ╔═╡ 2e12659d-c108-4cab-8b6b-19d2458b92f9
+M₃ = [[1, 3] [2, 4]]
+
 # ╔═╡ 0a8ac112-edef-11ea-1e99-cf7c7808c4f5
 typeof(M)
 
 # ╔═╡ 1295f48a-edef-11ea-22a5-61e8a2e1d005
-md"The `2` in the type confirms that this is a 2D array."
+md"Die `2` im Typen bestätigt, dass es sich tatsächlich um ein 2D `Array` handelt"
+
+# ╔═╡ 1d95f3ef-1163-44a5-a25e-55f9180d7395
+md"""
+Die Navigation innerhalb eines 2D-`Array`s ist identisch zum eindimensionalen Fall. Da wir nun in zwei Dimensionen arbeiten benötigen wir jedoch für die Position zwei `Indizes`. Der erste `Index` deklariert die Zeile in der wir arbeiten und der zweite `Index` die Spalte.
+
+`array[row, column]` bzw. deutsch: `array[Zeile, Spalte]`
+
+"""
+
+# ╔═╡ 7bbcbd5a-268c-481c-9974-f459140e13d9
+M
+
+# ╔═╡ b013ea63-458c-480a-8967-cd0b944df730
+M[1, 1]
+
+# ╔═╡ c544d9ba-25dd-4d05-8ab1-b6518b40b0f2
+M[2, 2]
+
+# ╔═╡ e4e8ae8d-db66-4fff-bf0c-883a7a8179e8
+md"""
+Natürlich funktioniert auch hier **slicing**.
+"""
+
+# ╔═╡ f4f3e45f-3ea2-4e42-acb1-3ababf16fe46
+M[1, 1:2]
+
+# ╔═╡ c2b3391e-c5fb-4620-a8c8-65167b389106
+M[1:2, 2]
+
+# ╔═╡ 1835d2f4-ea09-479c-9eda-46249619d685
+md"""
+Auch hier können wir natürlich Werte ändern.
+"""
+
+# ╔═╡ 5b08e2a7-9aac-4ec8-958f-3eac7da5f92c
+M[1:2, 2] = [7, 7]
+
+# ╔═╡ e592a31c-f76b-494a-af1e-c9e01e39eb66
+M
+
+# ╔═╡ 8414f2d0-324f-4c1a-8ef4-1af85a984172
+md"""
+### Vorgefertigte Arrays
+"""
 
 # ╔═╡ 3e1fdaa8-edef-11ea-2f03-eb41b2b9ea0f
-md"This won't work for larger matrices, though. For that we can use e.g."
+md"""
+Für große Matrizen ist es wenig sinnvoll, diese per Hand zu definieren. Hier greift man auf bereits vorgefertigte Konstruktionen von Julia zurück. Z.B. können wir eine Matrix (ein 2D `Array`) voller Nullen durch den Befehl
+
+`zeros(TYP, Anzahl_Zeilen, Anzahl_Spalten)`
+
+
+initialisieren. 
+"""
 
 # ╔═╡ 48f3deca-edef-11ea-2c18-e7419c9030a0
-zeros(5, 5)
+zeros(3, 5)
 
 # ╔═╡ a8f26af8-edef-11ea-2fc7-2b776f515aea
-md"Note that `zeros` gives `Float64`s by default. We can also specify a type for the elements:"
+md"Wenn wir kein TYP angeben, so geht Julia davon aus das wir Gleitkommazahlen `Float64` meinen. Möchten wir hingegen ganze Zahlen (`Integer`) so können wir dies durch den TYP festlegen."
 
 # ╔═╡ b595373e-edef-11ea-03e2-6599ef14af20
-zeros(Int, 4, 5)
+einmaleins = zeros(Int64, 5, 5)
 
 # ╔═╡ 4cb33c04-edef-11ea-2b35-1139c246c331
-md"We can then fill in the values we want by manipulating the elements, e.g. with a `for` loop."
+md"Jetzt könnte man mittels zweier in sich gekopppelter `for-Schleifen` über die  `Indizes` iterieren und somit die Matrix auffüllen. Hier z.B. eine 1×1- Tabelle bis 5: "
+
+# ╔═╡ 5faef391-1dbd-4187-9082-afc2051b49f5
+begin
+	
+for zeilen_idx in 1:5
+	for spalten_idx in 1:5
+		einmaleins[zeilen_idx, spalten_idx] = zeilen_idx * spalten_idx
+	end
+end
+einmaleins
+	
+end
+
+# ╔═╡ f5346ca1-c6de-4817-a6ac-40f1c06107a9
+md"""
+## Das kleine 1×1 mittels array comprehension
+"""
 
 # ╔═╡ 54e47e9e-edef-11ea-2d75-b5f550902528
-md"A nice alternative syntax to create matrices following a certain pattern is an array comprehension with a *double* `for` loop:"
+md"Eine sehr nette und elegante Alternative ist abermals das **array comprehension** Muster. Hier das 1×1 bis 12 durch eine einzige Zeile Code."
 
 # ╔═╡ 6348edce-edef-11ea-1ab4-019514eb414f
-[i + j for i in 1:5, j in 1:6]
+bigeinmaleins = [zeile * spalte for zeile in 1:12, spalte in 1:12]
 
 # ╔═╡ Cell order:
 # ╟─0d3aec92-edeb-11ea-3adb-cd0dc17cbdab
@@ -314,26 +493,59 @@ md"A nice alternative syntax to create matrices following a certain pattern is a
 # ╟─a16299a2-eded-11ea-2b56-93eb7a1010a7
 # ╠═bc6b124e-eded-11ea-0290-b3760cb81024
 # ╟─cfb21014-eded-11ea-1261-3bc30952a88e
+# ╠═0ba17e45-bdca-40de-af10-e4167251a5f7
+# ╟─22d6c86e-fbf2-4246-aee1-afe700b49355
 # ╟─ffee7d80-eded-11ea-26b1-1331df204c67
+# ╟─daeca7d5-0100-41ea-a106-6b1a7ecdcf7a
 # ╟─cae4137e-edee-11ea-14af-59a32227de1b
 # ╟─714f4fca-edee-11ea-3410-c9ab8825d836
 # ╠═82cc2a0e-edee-11ea-11b7-fbaa5ad7b556
 # ╠═85916c18-edee-11ea-0738-5f5d78875b86
 # ╟─881b7d0c-edee-11ea-0b4a-4bd7d5be2c77
 # ╠═a298e8ae-edee-11ea-3613-0dd4bae70c26
+# ╟─98664fb6-ebaf-449a-9f20-aaa3f955f44e
 # ╠═a5ebddd6-edee-11ea-2234-55453ea59c5a
-# ╟─a9b48e54-edee-11ea-1333-a96181de0185
+# ╠═b6a3fe2e-7504-4d94-9672-ef564308d387
+# ╟─3c7424db-51c9-41a2-9630-2c188a80d5d5
 # ╟─68c4ead2-edef-11ea-124a-03c2d7dd6a1b
 # ╠═84129294-edef-11ea-0c77-ffa2b9592a26
+# ╟─0025dea4-99cc-4cb4-9c92-f14d04af56eb
+# ╠═de160dfe-53ef-4788-86df-157b44e5e1da
+# ╟─4341aee5-0631-4171-953c-2ba07e0ee135
+# ╠═3e362465-9e27-45b2-a062-757cbde31eca
+# ╟─cbd56fc2-1c96-45fb-99a9-2deb68a0e2b9
+# ╟─2a23488b-49bf-4743-9501-21030acc2139
+# ╠═c386d317-3fdd-4088-b5e7-cc7dadd0b518
+# ╟─b55c6ef3-c81d-4f6a-8f0f-05044d751a9d
+# ╠═b26ff5b6-6e68-40e6-9ef1-afa2105f268b
+# ╟─45bba432-2b07-4a72-87e4-688bb2ca40a9
+# ╟─d5406f4c-023b-4e96-9707-10436b96df56
+# ╠═d13638ef-1837-44a0-afb9-7bedcb7187b9
 # ╟─d364fa16-edee-11ea-2050-0f6cb70e1bcf
 # ╟─db99ae9a-edee-11ea-393e-9de420a545a1
 # ╠═04f175f2-edef-11ea-0882-712548ebb7a3
+# ╠═bc19f4aa-404f-48df-ad5f-bfceed049286
+# ╟─a3881a8c-1bbd-4e42-96af-f8b5786c9cac
+# ╠═2e12659d-c108-4cab-8b6b-19d2458b92f9
 # ╠═0a8ac112-edef-11ea-1e99-cf7c7808c4f5
 # ╟─1295f48a-edef-11ea-22a5-61e8a2e1d005
+# ╟─1d95f3ef-1163-44a5-a25e-55f9180d7395
+# ╠═7bbcbd5a-268c-481c-9974-f459140e13d9
+# ╠═b013ea63-458c-480a-8967-cd0b944df730
+# ╠═c544d9ba-25dd-4d05-8ab1-b6518b40b0f2
+# ╟─e4e8ae8d-db66-4fff-bf0c-883a7a8179e8
+# ╠═f4f3e45f-3ea2-4e42-acb1-3ababf16fe46
+# ╠═c2b3391e-c5fb-4620-a8c8-65167b389106
+# ╟─1835d2f4-ea09-479c-9eda-46249619d685
+# ╠═5b08e2a7-9aac-4ec8-958f-3eac7da5f92c
+# ╠═e592a31c-f76b-494a-af1e-c9e01e39eb66
+# ╟─8414f2d0-324f-4c1a-8ef4-1af85a984172
 # ╟─3e1fdaa8-edef-11ea-2f03-eb41b2b9ea0f
 # ╠═48f3deca-edef-11ea-2c18-e7419c9030a0
 # ╟─a8f26af8-edef-11ea-2fc7-2b776f515aea
 # ╠═b595373e-edef-11ea-03e2-6599ef14af20
 # ╟─4cb33c04-edef-11ea-2b35-1139c246c331
+# ╠═5faef391-1dbd-4187-9082-afc2051b49f5
+# ╟─f5346ca1-c6de-4817-a6ac-40f1c06107a9
 # ╟─54e47e9e-edef-11ea-2d75-b5f550902528
 # ╠═6348edce-edef-11ea-1ab4-019514eb414f
